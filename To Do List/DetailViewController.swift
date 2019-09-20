33d2666be82eea6d69bb8778cfab64340f3b67cb
Anastasia Kyratzoglou <kyratzoa@bc.edu>
2019-09-19 23:22:50 -0400

    5.5 Completed - Interface Polish—Show Keyboard with BecomeFirstResponder and Disable Buttons When Text Field is Empty

--- To Do List/DetailViewController.swift
+++ To Do List/DetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var toDoField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
     var toDoItem: String?
     
     override func viewDidLoad() {
@@ -18,13 +19,26 @@ class DetailViewController: UIViewController {
         if let toDoItem = toDoItem {
             toDoField.text = toDoItem
         }
+        
+        toDoField.becomeFirstResponder()
+        enableDisableSaveButton()
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave"{
             toDoItem = toDoField.text
         }
     }
+    func enableDisableSaveButton(){
+        if let toDoFieldCount = toDoField.text?.count, toDoFieldCount > 0{
+            saveBarButton.isEnabled = true
+        }else{
+            saveBarButton.isEnabled = false
+        }
+    }
     
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        enableDisableSaveButton()
+    }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
